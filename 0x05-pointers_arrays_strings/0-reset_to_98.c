4bef72ef9620b7e0eaecff87c5f32b6d6eeb2e2a
Cobbyheino <danso.cobby@gmail.com>
2023-06-27 07:10:43 -0700

    first push

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takkes a pointer to an int as parameter
+ * and updates the value to 96
+ * @n:input
+ * return: always 0
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
