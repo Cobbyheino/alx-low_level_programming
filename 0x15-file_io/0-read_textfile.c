ae5cb440e3c6a74bbb7e00d09e5f37c48152f9c8
Cobbyheino <danso.cobby@gmail.com>
2023-10-02 17:13:16 -0700

    first push task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - function that reads a text file
+ * and prints to STDOUT.
+ * @filename: this is the file name
+ * @letters: number of letters
+ * Return: number of letters or 0 if function fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+
+{
+	char *buffer;
+	ssize_t fd;
+	ssize_t rite;
+	ssize_t reed;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	return (0);
+	buffer = malloc(sizeof(char) * letters);
+	reed = read(fd, buffer, letters);
+	rite = write(STDOUT_FILENO, buffer, reed);
+
+	free(buffer);
+	close(fd);
+	return (rite);
+}
