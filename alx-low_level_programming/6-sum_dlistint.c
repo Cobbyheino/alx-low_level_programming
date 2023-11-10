18f8ff2a97a74ec7f05ed7e4968d4113fc8b0085
Cobbyheino <danso.cobby@gmail.com>
2023-11-10 02:51:31 +0000

    task6

---
+++ alx-low_level_programming/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums up data
+ * @head: headoflist
+ * Return: sum of values
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
